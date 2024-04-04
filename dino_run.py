02e298c658f39babbc990b74d996ffb62205a516
joliecannoli <keoanj12@gmail.com>
2024-04-03 17:24:46 -1000

    Added dinosaur ducking function and animation

--- dino_run.py
+++ dino_run.py
@@ -26,6 +26,7 @@ CLOUDS = pygame.image.load(os.path.join("pngs/others", "cloud.png"))
 class Dinosaur: 
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340 
 
     def __init__ (self):
         self.duck_img = DUCKING
@@ -67,7 +68,11 @@ class Dinosaur:
             self.dino_jump = False
 
     def duck(self):
-        pass
+        self.image = self.duck_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
