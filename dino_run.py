951b65d85779acf4cce01eda051a8cbc404951a4
joliecannoli <keoanj12@gmail.com>
2024-04-03 17:44:26 -1000

    Added if statement to make clouds appear again when they move off screen

--- dino_run.py
+++ dino_run.py
@@ -96,12 +96,20 @@ class Dinosaur:
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
-class Cloud:
+class Clouds:
     def __init__ (self):
         self.x = SCREEN_WIDTH + random.randint(800, 1000)
         self.y = random.randint(50, 100)
         self.image = CLOUDS
         self.width = self.image.get_width()
+
+        def update(self):
+            self.x -=game_speed 
+            if self.x < -self.width:
+                self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+                self.y = random.randint(50, 100)
+
+    
 
 def main():
     global game_speed
